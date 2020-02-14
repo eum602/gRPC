ef06ca226c58a1a433851448ea783988b2abb70c
Erick <eum602@gmail.com>
2020-02-13 19:09:23 -0500

    created a simple instance of a .proto file

--- protobuf-examples/java/protobuf-example-1/src/main/java/com/eum602/protobuf/SimpleMain.java
+++ protobuf-examples/java/protobuf-example-1/src/main/java/com/eum602/protobuf/SimpleMain.java
@@ -1,7 +1,17 @@
 package com.eum602.protobuf;
 
+import my.job.JobOuterClass.Job;
+
 public class SimpleMain {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        Job.Builder builder = Job.newBuilder();//creating a new instance of Job proto, which was created before
+        //by simply running a hello world here.
+        //setting a company name for the new Job instance
+        builder.setCompany("Acme");
+        builder.setExperienceTime(3);
+        builder.setDescription("Bsc Mechatronics Engineering");
+        builder.setIsCurrentlyWorking(true);
     }
 }
